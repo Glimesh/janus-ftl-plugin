Glimesh/janus-ftl-plugin

commit ad2ab7867138b9efb42ed486087751e1a68e5774

📚 Update cpp-httplib, log error code on failed HTTP requests (#81)
This change updates us to `v0.8.4` of [cpp-httplib](https://github.com/yhirose/cpp-httplib), which includes a decent number of fixes to bugs that I suspect may be causing some of our HTTPS request failures we're seeing in production.

As a result of some of the changes in the httplib update, a few changes were made to the ServiceConnection implementations to avoid copying of `httplib::Client` and `httplib::Result` instances (they are now backed by `unique_ptr`).

Additional logging detail was also added in the Glimesh HTTP request failure case so we can determine root cause if this doesn't resolve the failures.

Verified by testing against local glimesh.tv instance.

src/ServiceConnections/RestServiceConnection.cpp
@@ -24,12 +24,22 @@ RestServiceConnection::RestServiceConnection(
     std::string pathBase,
     std::string authToken)
 :
+    httpClient(createBaseUri(false).c_str()),
     hostname(hostname),
     port(port),
     useHttps(useHttps),
     pathBase(pathBase),
     authToken(authToken)
-{ }
+{
+    if (authToken.length() > 0)
+    {
+        httplib::Headers headers
+        {
+            {"Authorization", authToken}
+        };
+        httpClient.set_default_headers(headers);
+    }
+}
 #pragma endregion
 
 #pragma region Public methods
@@ -203,30 +213,12 @@ std::string RestServiceConnection::constructPath(std::string path)
     return ss.str();
 }
 
-httplib::Client RestServiceConnection::getHttpClient()
-{
-    std::string baseUri = createBaseUri(false);
-    httplib::Client client = httplib::Client(baseUri.c_str());
-
-    if (authToken.length() > 0)
-    {
-        httplib::Headers headers
-        {
-            {"Authorization", authToken}
-        };
-        client.set_default_headers(headers);
-    }
-
-    return client;
-}
-
 httplib::Result RestServiceConnection::runGetRequest(std::string url)
 {
     // Make the request, and retry if necessary
     int numRetries = 0;
     while (true)
     {
-        httplib::Client httpClient = getHttpClient();
         std::string fullUrl = constructPath(url);
 
         httplib::Result response = httpClient.Get(fullUrl.c_str());
@@ -277,7 +269,6 @@ httplib::Result RestServiceConnection::runPostRequest(
     int numRetries = 0;
     while (true)
     {
-        httplib::Client httpClient = getHttpClient();
         std::string fullUrl = constructPath(url);
 
         httplib::Result response = (fileData.size() > 0)
@@ -312,7 +303,7 @@ httplib::Result RestServiceConnection::runPostRequest(
     throw ServiceConnectionCommunicationFailedException("REST POST request failed.");
 }
 
-JsonPtr RestServiceConnection::decodeRestResponse(httplib::Result result)
+JsonPtr RestServiceConnection::decodeRestResponse(const httplib::Result& result)
 {
     if (result)
     {

src/ServiceConnections/RestServiceConnection.h
@@ -45,6 +45,7 @@ private:
     /* Private members */
     const int MAX_RETRIES = 5;
     const int TIME_BETWEEN_RETRIES_MS = 3000;
+    httplib::Client httpClient;
     std::string hostname;
     uint16_t port;
     bool useHttps;
@@ -56,8 +57,8 @@ private:
     std::string createBaseUri(bool includeBase);
     std::string constructPath(std::string path);
 
-    httplib::Client getHttpClient();
     httplib::Result runGetRequest(std::string url);
-    httplib::Result runPostRequest(std::string url, JsonPtr body = nullptr, httplib::MultipartFormDataItems fileData = httplib::MultipartFormDataItems());
-    JsonPtr decodeRestResponse(httplib::Result result);
+    httplib::Result runPostRequest(std::string url, JsonPtr body = nullptr,
+        httplib::MultipartFormDataItems fileData = httplib::MultipartFormDataItems());
+    JsonPtr decodeRestResponse(const httplib::Result& result);
 };

src/ServiceConnections/GlimeshServiceConnection.cpp
@@ -12,6 +12,7 @@
 
 #include "../Utilities/FtlTypes.h"
 
+#include <fmt/core.h>
 #include <jansson.h>
 #include <string.h>
 #include <spdlog/spdlog.h>
@@ -23,6 +24,7 @@ GlimeshServiceConnection::GlimeshServiceConnection(
     bool useHttps,
     std::string clientId,
     std::string clientSecret) : 
+    httpClient(fmt::format("{}://{}:{}", (useHttps ? "https" : "http"), hostname, port).c_str()),
     hostname(hostname),
     port(port),
     useHttps(useHttps),
@@ -193,30 +195,11 @@ Result<void> GlimeshServiceConnection::SendJpegPreviewImage(
 #pragma endregion
 
 #pragma region Private methods
-httplib::Client GlimeshServiceConnection::getHttpClient()
-{
-    std::stringstream baseUri;
-    baseUri << (useHttps ? "https" : "http") << "://" << hostname << ":" << port;
-    httplib::Client client = httplib::Client(baseUri.str().c_str());
-
-    if (accessToken.length() > 0)
-    {
-        httplib::Headers headers
-        {
-            {"Authorization", "Bearer " + accessToken}
-        };
-        client.set_default_headers(headers);
-    }
-
-    return client;
-}
-
 void GlimeshServiceConnection::ensureAuth()
 {
     std::lock_guard<std::mutex> lock(authMutex);
 
     // Do we already have an access token that hasn't expired?
-    // TODO: Check expiration
     if (accessToken.length() > 0)
     {
         std::time_t currentTime = std::time(nullptr);
@@ -235,7 +218,6 @@ void GlimeshServiceConnection::ensureAuth()
         { "scope", "streamkey" }
     };
 
-    httplib::Client httpClient = getHttpClient();
     if (httplib::Result res = httpClient.Post("/api/oauth/token", params))
     {
         if (res->status == 200)
@@ -267,6 +249,13 @@ void GlimeshServiceConnection::ensureAuth()
                 std::time_t currentTime = std::time(nullptr);
                 spdlog::info("Received new access token: {}, expires in {} - {} = {} seconds",
                     accessToken, expirationTime, currentTime, (expirationTime - currentTime));
+
+                // Update HTTP client Authorization header
+                httplib::Headers headers
+                {
+                    {"Authorization", "Bearer " + accessToken}
+                };
+                httpClient.set_default_headers(headers);
                 return;
             }
         }
@@ -311,7 +300,6 @@ JsonPtr GlimeshServiceConnection::runGraphQlQuery(
     int numRetries = 0;
     while (true)
     {
-        httplib::Client httpClient = getHttpClient();
         JsonPtr result = nullptr;
 
         // If we're doing files, use a multipart http request
@@ -334,7 +322,7 @@ JsonPtr GlimeshServiceConnection::runGraphQlQuery(
 
         if (numRetries < MAX_RETRIES)
         {
-            spdlog::warn("Attempt {} / {}: Glimesh file upload GraphQL query failed. "
+            spdlog::warn("Attempt {} / {}: Glimesh GraphQL query failed. "
                 "Retrying in {} ms...", (numRetries + 1), MAX_RETRIES, TIME_BETWEEN_RETRIES_MS);
 
             std::this_thread::sleep_for(std::chrono::milliseconds(TIME_BETWEEN_RETRIES_MS));
@@ -347,13 +335,13 @@ JsonPtr GlimeshServiceConnection::runGraphQlQuery(
     }
     
     // We've exceeded our retry limit
-    spdlog::error("Aborting Glimesh file upload GraphQL query after {} failed attempts.",
+    spdlog::error("Aborting Glimesh GraphQL query after {} failed attempts.",
         MAX_RETRIES);
 
     throw ServiceConnectionCommunicationFailedException("Glimesh GraphQL query failed.");
 }
 
-JsonPtr GlimeshServiceConnection::processGraphQlResponse(httplib::Result result)
+JsonPtr GlimeshServiceConnection::processGraphQlResponse(const httplib::Result& result)
 {
     if (result)
     {
@@ -382,7 +370,8 @@ JsonPtr GlimeshServiceConnection::processGraphQlResponse(httplib::Result result)
     }
     else
     {
-        spdlog::warn("Glimesh service connection HTTP request failed.");
+        spdlog::warn("Glimesh service connection HTTP request failed with error {}",
+            result.error());
         return nullptr;
     }
 }

src/ServiceConnections/GlimeshServiceConnection.h
@@ -47,8 +47,9 @@ public:
 
 private:
     /* Private members */
-    const int MAX_RETRIES = 5;
+    const int MAX_RETRIES = 10;
     const int TIME_BETWEEN_RETRIES_MS = 3000;
+    httplib::Client httpClient;
     std::string hostname;
     uint16_t port;
     bool useHttps;
@@ -59,9 +60,8 @@ private:
     std::mutex authMutex;
 
     /* Private methods */
-    httplib::Client getHttpClient();
     void ensureAuth();
     JsonPtr runGraphQlQuery(std::string query, JsonPtr variables = nullptr, httplib::MultipartFormDataItems fileData = httplib::MultipartFormDataItems());
-    JsonPtr processGraphQlResponse(httplib::Result result);
+    JsonPtr processGraphQlResponse(const httplib::Result& result);
     tm parseIso8601DateTime(std::string dateTimeString);
 };

commit 7f15243d5fb4c24d58acd9c0db4f8fc8206baaf2

💤 Fix crash in REST service connection (#82)
Fixes a crash in the RestServiceConnection that was introduced by #81

The member initializer list was attempting to initialize the `httpClient` member with other member values that had not yet been initialized themselves.

Did a little bit of cleanup as well, replacing `stringstream` with `fmt::format`.

src/ServiceConnections/RestServiceConnection.cpp
@@ -12,6 +12,8 @@
 
 #include "../Utilities/FtlTypes.h"
 
+#include <cassert>
+#include <fmt/core.h>
 #include <jansson.h>
 #include <spdlog/spdlog.h>
 #include <sstream>
@@ -24,14 +26,27 @@ RestServiceConnection::RestServiceConnection(
     std::string pathBase,
     std::string authToken)
 :
-    httpClient(createBaseUri(false).c_str()),
+    httpClient(getHostUrl(useHttps, hostname, port).c_str()),
     hostname(hostname),
     port(port),
     useHttps(useHttps),
     pathBase(pathBase),
     authToken(authToken)
 {
-    if (authToken.length() > 0)
+    // Ensure our path base has a starting slash and ending slash
+    if (this->pathBase.size() > 0)
+    {
+        if (this->pathBase.front() != '/')
+        {
+            this->pathBase.insert(this->pathBase.begin(), '/');
+        }
+        if (this->pathBase.back() != '/')
+        {
+            this->pathBase.push_back('/');
+        }
+    }
+
+    if (this->authToken.length() > 0)
     {
         httplib::Headers headers
         {
@@ -45,16 +60,13 @@ RestServiceConnection::RestServiceConnection(
 #pragma region Public methods
 void RestServiceConnection::Init()
 {
-    std::string baseUri = createBaseUri(true);
-    spdlog::info("Using REST Service Connection @ {}", baseUri);
+    spdlog::info("Using REST Service Connection @ {}{}", getHostUrl(useHttps, hostname, port),
+        pathBase);
 }
 
 Result<std::vector<std::byte>> RestServiceConnection::GetHmacKey(ftl_channel_id_t channelId)
 {
-    std::stringstream url;
-    url << "/hmac/" << channelId;
-
-    httplib::Result response = runGetRequest(url.str());
+    httplib::Result response = runGetRequest(fmt::format("hmac/{}", channelId));
     if (response->status >= 400)
     {
         return Result<std::vector<std::byte>>::Error(
@@ -78,16 +90,11 @@ Result<std::vector<std::byte>> RestServiceConnection::GetHmacKey(ftl_channel_id_
 
 Result<ftl_stream_id_t> RestServiceConnection::StartStream(ftl_channel_id_t channelId)
 {
-    std::stringstream url;
-    url << "/start/" << channelId;
-
-    httplib::Result response = runPostRequest(url.str());
+    httplib::Result response = runPostRequest(fmt::format("start/{}", channelId));
     if (response->status >= 400)
     {
-        std::stringstream errMsg;
-        errMsg << "Channel " << channelId << " is not allowed to stream.";
-
-        return Result<ftl_stream_id_t>::Error(errMsg.str().c_str());
+        return Result<ftl_stream_id_t>::Error(
+            fmt::format("Channel {} is not allowed to stream.", channelId));
     }
 
     JsonPtr result = decodeRestResponse(response);
@@ -124,19 +131,14 @@ Result<void> RestServiceConnection::UpdateStreamMetadata(ftl_stream_id_t streamI
         "videoWidth",        metadata.videoWidth
     ));
 
-    std::stringstream url;
-    url << "/metadata/" << streamId;
-
-    runPostRequest(url.str(), std::move(streamMetadata));
+    runPostRequest(fmt::format("metadata/{}", streamId), std::move(streamMetadata));
     return Result<void>::Success();
 }
 
 Result<void> RestServiceConnection::EndStream(ftl_stream_id_t streamId)
 {
-    std::stringstream url;
-    url << "/end/" << streamId;
-    runPostRequest(url.str());
-
+    runPostRequest(fmt::format("end/{}", streamId));
+    // TOOD: Handle errors
     return Result<void>::Success();
 }
 
@@ -154,10 +156,7 @@ Result<void> RestServiceConnection::SendJpegPreviewImage(
             .content_type = "image/jpeg"
         }
     };
-
-    std::stringstream url;
-    url << "/preview/" << streamId;
-    runPostRequest(url.str(), nullptr, files);
+    runPostRequest(fmt::format("preview/{}", streamId), nullptr, files);
 
     // TODO: Handle errors
     return Result<void>::Success();
@@ -165,63 +164,26 @@ Result<void> RestServiceConnection::SendJpegPreviewImage(
 #pragma endregion
 
 #pragma region Private methods
-std::string RestServiceConnection::resolvePathBase()
+std::string RestServiceConnection::getHostUrl(bool https, std::string hostname, uint16_t port)
 {
-    if (pathBase == "/")
-    {
-        return "";
-    }
-
-    // Check for leading slash and insert if needed
-    std::stringstream ss;
-    if (pathBase.rfind("/", 0) == std::string::npos)
-    {
-        ss << "/" << pathBase;
-    }
-    else
-    {
-        ss << pathBase;
-    }
-
-    // Trim trailing slash
-    std::string newBase = ss.str();
-    if (newBase.find("/", newBase.size() - 1) != std::string::npos)
-    {
-        newBase.pop_back();
-    }
-
-    return newBase;
+    return fmt::format("{}://{}:{}", (https ? "https" : "http"), hostname, port);
 }
 
-std::string RestServiceConnection::createBaseUri(bool includeBase)
+std::string RestServiceConnection::relativeToAbsolutePath(std::string relativePath)
 {
-    std::stringstream baseUri;
-    baseUri << (useHttps ? "https" : "http") << "://" << hostname << ":" << port;
-    if (includeBase)
-    {
-        baseUri << resolvePathBase();
-    }
-
-    return baseUri.str();
+    // Relative path must not be prefixed with a slash
+    assert((relativePath.size() > 0) && (relativePath.front() != '/'));
+    return fmt::format("{}{}", pathBase, relativePath);
 }
 
-std::string RestServiceConnection::constructPath(std::string path)
-{
-    std::stringstream ss;
-    ss << resolvePathBase() << path;
-
-    return ss.str();
-}
-
-httplib::Result RestServiceConnection::runGetRequest(std::string url)
+httplib::Result RestServiceConnection::runGetRequest(std::string path)
 {
     // Make the request, and retry if necessary
     int numRetries = 0;
     while (true)
     {
-        std::string fullUrl = constructPath(url);
-
-        httplib::Result response = httpClient.Get(fullUrl.c_str());
+        std::string absolutePath = relativeToAbsolutePath(path);
+        httplib::Result response = httpClient.Get(absolutePath.c_str());
         if (response && response.error() == httplib::Error::Success && response->status < 500)
         {
             return response;
@@ -248,34 +210,32 @@ httplib::Result RestServiceConnection::runGetRequest(std::string url)
     throw ServiceConnectionCommunicationFailedException("REST GET request failed.");
 }
 
-httplib::Result RestServiceConnection::runPostRequest(
-    std::string url,
-    JsonPtr body,
+httplib::Result RestServiceConnection::runPostRequest(std::string path, JsonPtr body,
     httplib::MultipartFormDataItems fileData)
 {
-    std::string bodyString;
-    if (body)
-    {
-        char* bodyStr = json_dumps(body.get(), 0);
-        bodyString = std::string(bodyStr);
-        free(bodyStr);
-    }
-    else
-    {
-        bodyString = "";
-    }
-
     // Make the request, and retry if necessary
     int numRetries = 0;
     while (true)
     {
-        std::string fullUrl = constructPath(url);
-
-        httplib::Result response = (fileData.size() > 0)
-            ? httpClient.Post(fullUrl.c_str(), fileData)
-            : (body)
-            ? httpClient.Post(fullUrl.c_str(), bodyString, "application/json")
-            : httpClient.Post(fullUrl.c_str(), "", "text/plain");
+        std::string absolutePath = relativeToAbsolutePath(path);
+        httplib::Result response = [&]
+        {
+            if (fileData.size() > 0)
+            {
+                return httpClient.Post(absolutePath.c_str(), fileData);
+            }
+            else if (body)
+            {
+                char* bodyStr = json_dumps(body.get(), 0);
+                auto bodyString = std::string(bodyStr);
+                free(bodyStr);
+                return httpClient.Post(absolutePath.c_str(), bodyString, "application/json");
+            }
+            else
+            {
+                return httpClient.Post(absolutePath.c_str(), "", "text/plain");
+            }
+        }();
 
         if (response && response.error() == httplib::Error::Success && response->status < 500)
         {
@@ -316,10 +276,9 @@ JsonPtr RestServiceConnection::decodeRestResponse(const httplib::Result& result)
             {
                 // If we can't parse the JSON of a successful request, retrying won't help,
                 // so we throw here.
-                std::stringstream errStr;
-                errStr << "Could not parse JSON response from REST Service Connection: \n"
-                    << result->body.c_str();
-                throw ServiceConnectionCommunicationFailedException(errStr.str().c_str());
+                throw ServiceConnectionCommunicationFailedException(
+                    fmt::format("Could not parse JSON response from REST Service Connection: \n{}",
+                        result->body).c_str());
             }
             return jsonBody;
         }

src/ServiceConnections/RestServiceConnection.h
@@ -53,12 +53,10 @@ private:
     std::string authToken;
 
     /* Private methods */
-    std::string resolvePathBase();
-    std::string createBaseUri(bool includeBase);
-    std::string constructPath(std::string path);
-
-    httplib::Result runGetRequest(std::string url);
-    httplib::Result runPostRequest(std::string url, JsonPtr body = nullptr,
+    std::string getHostUrl(bool https, std::string hostname, uint16_t port);
+    std::string relativeToAbsolutePath(std::string relativePath);
+    httplib::Result runGetRequest(std::string path);
+    httplib::Result runPostRequest(std::string path, JsonPtr body = nullptr,
         httplib::MultipartFormDataItems fileData = httplib::MultipartFormDataItems());
     JsonPtr decodeRestResponse(const httplib::Result& result);
 };