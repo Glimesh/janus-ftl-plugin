d2e2e3279d547fb63f5baec7c360a3dadc0ff764
Daniel Stiner <danstiner@gmail.com>
2021-04-18 21:48:39 -0700

    Back to test main

--- test/test.cpp
+++ test/test.cpp
@@ -9,5 +9,12 @@
 // https://github.com/catchorg/Catch2/blob/v2.x/docs/ci-and-misc.md#precompiled-headers-pchs
 #undef TWOBLUECUBES_SINGLE_INCLUDE_CATCH_HPP_INCLUDED
 #define CATCH_CONFIG_IMPL_ONLY
-#define CATCH_CONFIG_MAIN
+#define CATCH_CONFIG_RUNNER
 #include <catch2/catch.hpp>
+
+int main(int argc, char* argv[])
+{
+    // Test!
+    int result = Catch::Session().run(argc, argv);
+    return result;
+}
