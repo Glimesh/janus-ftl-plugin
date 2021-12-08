b434820bc1535127939b914e51caa147bd0bec7a
Daniel Stiner <danstiner@gmail.com>
2021-12-07 23:43:58 -0800

    Formatting

--- src/Rtp/ExtendedSequenceCounter.cpp
+++ src/Rtp/ExtendedSequenceCounter.cpp
@@ -9,7 +9,7 @@
 
 #include "ExtendedSequenceCounter.h"
 
-bool ExtendedSequenceCounter::Extend(rtp_sequence_num_t seq, rtp_extended_sequence_num_t* extendedSeq)
+bool ExtendedSequenceCounter::Extend(rtp_sequence_num_t seq, rtp_extended_sequence_num_t *extendedSeq)
 {
     bool valid = UpdateState(seq);
     *extendedSeq = cycles | seq;
@@ -23,13 +23,13 @@ bool ExtendedSequenceCounter::UpdateState(rtp_sequence_num_t seq)
         Initialize(seq);
         return true;
     }
-    
+
     uint16_t udelta = seq - maxSeq;
 
     /*
-    * Source is not considered stable until MIN_SEQUENTIAL packets with
-    * sequential sequence numbers have been received.
-    */
+     * Source is not considered stable until MIN_SEQUENTIAL packets with
+     * sequential sequence numbers have been received.
+     */
     if (probation > 0)
     {
         /* packet is in sequence */
@@ -50,34 +50,47 @@ bool ExtendedSequenceCounter::UpdateState(rtp_sequence_num_t seq)
             maxSeq = seq;
         }
         /*
-        * Unlike RFC 3550, we consider a packet valid even if less than MIN_SEQUENTIAL
-        * have been received as long it is sequential to all packets received so far.
-        */ 
+         * Unlike RFC 3550, we consider a packet valid even if less than MIN_SEQUENTIAL
+         * have been received as long it is sequential to all packets received so far.
+         */
         return true;
-    } else if (udelta <= MAX_DROPOUT) {
+    }
+    /* 
+     * Allow small jumps due to lost packets. We allow up to and including for simpler
+     * reasoning, the RFC 3550 example is slightly different and does (udelta < MAX_DROPOUT)
+     */
+    else if (udelta <= MAX_DROPOUT)
+    {
         /* in order, with permissible gap */
-        if (seq < maxSeq) {
+        if (seq < maxSeq)
+        {
             /*
-            * Sequence number wrapped - count another 64K cycle.
-            */
+             * Sequence number wrapped - count another 64K cycle.
+             */
             cycles += RTP_SEQ_MOD;
         }
         maxSeq = seq;
-    } else if (udelta <= RTP_SEQ_MOD - MAX_MISORDER) {
+    }
+    else if (udelta <= RTP_SEQ_MOD - MAX_MISORDER)
+    {
         /* the sequence number made a very large jump */
-        if (seq == badSeq) {
+        if (seq == badSeq)
+        {
             /*
-            * Two sequential packets -- assume that the other side
-            * restarted without telling us so just re-sync
-            * (i.e., pretend this was the first packet).
-            */
+             * Two sequential packets -- assume that the other side
+             * restarted without telling us so just re-sync
+             * (i.e., pretend this was the first packet).
+             */
             Reset(seq);
         }
-        else {
-            badSeq = (seq + 1) & (RTP_SEQ_MOD-1);
+        else
+        {
+            badSeq = (seq + 1) & (RTP_SEQ_MOD - 1);
             return false;
         }
-    } else {
+    }
+    else
+    {
         /* duplicate or reordered packet */
     }
     received++;
@@ -96,20 +109,19 @@ void ExtendedSequenceCounter::Reset(rtp_sequence_num_t seq)
 {
     baseSeq = seq;
     maxSeq = seq;
-    badSeq = RTP_SEQ_MOD + 1;   /* so seq == badSeq is false */
+    badSeq = RTP_SEQ_MOD + 1; /* so seq == badSeq is false */
     cycles = 0;
     received = 0;
     receivedPrior = 0;
     expectedPrior = 0;
 }
 
-std::ostream& operator<<(std::ostream & os, const ExtendedSequenceCounter& c)
+std::ostream &operator<<(std::ostream &os, const ExtendedSequenceCounter &c)
 {
-    os << "ExtendedSequenceCounter { " <<
-        "maxSeq:"   << c.maxSeq   << ", " <<
-        "cycles:"   << c.cycles   << ", " <<
-        "baseSeq:"  << c.baseSeq  << ", " <<
-        "received:" << c.received <<
-        " }";
+    os << "ExtendedSequenceCounter { "
+       << "maxSeq:" << c.maxSeq << ", "
+       << "cycles:" << c.cycles << ", "
+       << "baseSeq:" << c.baseSeq << ", "
+       << "received:" << c.received << " }";
     return os;
 }
