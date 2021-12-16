3fb812296a22ca33c316f3edb6340d1bd6f2e5e6
Daniel Stiner <danstiner@gmail.com>
2021-12-16 00:58:28 -0800

    Cleanup tests a bit

--- test/unit/Rtp/ExtendedSequenceCounterTests.cpp
+++ test/unit/Rtp/ExtendedSequenceCounterTests.cpp
@@ -27,102 +27,113 @@ std::string to_string( const T& value )
 void extend(
     ExtendedSequenceCounter& counter,
     rtp_sequence_num_t seq,
-    rtp_extended_sequence_num_t expectedExtendedSeq,
+    rtp_extended_sequence_num_t expected,
     bool expectValid = true)
 {
     auto result = counter.Extend(seq);
+    rtp_extended_sequence_num_t extended = result.extendedSeq;
 
-    UNSCOPED_INFO("extend - seq:" << seq << " extended:" << result.extendedSeq << " valid:" << result.valid);
-    UNSCOPED_INFO("" << counter);
-    REQUIRE(result.extendedSeq == expectedExtendedSeq);
+    CAPTURE(seq, extended, expected, result.valid, counter);
+    REQUIRE(extended == expected);
     CHECK(result.valid == expectValid);
 }
 
 TEST_CASE("Sequence from zero is valid")
 {
     ExtendedSequenceCounter counter;
-    rtp_extended_sequence_num_t extended = 0;
+    rtp_extended_sequence_num_t seq = 0;
     for (int i = 0; i < 100; ++i)
     {
-        extend(counter, extended, extended);
-        extended++;
+        extend(counter, seq, seq);
+        seq++;
     }
 }
 
 TEST_CASE("Sequence that wraps is valid")
 {
     ExtendedSequenceCounter counter;
-    rtp_extended_sequence_num_t extended = MAX_SEQ_NUM - 50;
+    rtp_extended_sequence_num_t seq = MAX_SEQ_NUM - 50;
     for (int i = 0; i < 100; ++i)
     {
-        extend(counter, extended, extended);
-        extended++;
+        extend(counter, seq, seq);
+        seq++;
     }
 }
 
 TEST_CASE("A skip less than MAX_DROPOUT is treated as valid")
 {
     ExtendedSequenceCounter counter;
-    rtp_extended_sequence_num_t extended = 0;
+    rtp_extended_sequence_num_t seq = 0;
+
+    INFO("Send MIN_SEQUENTIAL packets to initialize counter");
     for (int i = 0; i <= ExtendedSequenceCounter::MIN_SEQUENTIAL; ++i)
     {
-        extend(counter, extended, extended);
-        extended++;
+        extend(counter, seq, seq);
+        seq++;
     }
 
-    // Skip ahead less than MAX_DROPOUT
-    extended += ExtendedSequenceCounter::MAX_DROPOUT - 1;
+    INFO("Skip less than MAX_DROPOUT packets");
+    CAPTURE(seq, counter);
+    seq += ExtendedSequenceCounter::MAX_DROPOUT - 1;
 
+    INFO("Send a few more packets");
     for (int i = 0; i < 10; ++i)
     {
-        extend(counter, extended, extended);
-        extended++;
+        extend(counter, seq, seq);
+        seq++;
     }
 }
 
 TEST_CASE("A small skip accross a wrap is valid")
 {
     ExtendedSequenceCounter counter;
-    rtp_extended_sequence_num_t extended = MAX_SEQ_NUM - 50;
-    for (int i = 0; i < 10; ++i, extended++)
+    rtp_extended_sequence_num_t seq = MAX_SEQ_NUM - 50;
+
+    INFO("Send a few packets");
+    for (int i = 0; i < 10; ++i)
     {
-        extend(counter, extended, extended);
+        extend(counter, seq, seq);
+        seq++;
     }
 
-    // Skip ahead less than MIN_DROPOUT but enough to wrap-around
-    extended += 100;
+    INFO("Skip ahead less than MIN_DROPOUT but enough to wrap-around");
+    CAPTURE(seq, counter);
+    seq += 100;
 
-    for (int i = 0; i < 10; ++i, ++extended)
+    for (int i = 0; i < 10; ++i)
     {
-        extend(counter, extended, extended);
+        extend(counter, seq, seq);
+        seq++;
     }
 }
 
 TEST_CASE("NACKs should not reset sequence counter")
 {
     ExtendedSequenceCounter counter;
-    rtp_extended_sequence_num_t extended = 0;
+    rtp_extended_sequence_num_t seq = 0;
 
-    // Run sequence for a bit
+    INFO("Send a few packets");
     for (int i = 0; i < 100; ++i)
     {
-        extend(counter, extended, extended);
-        extended++;
+        extend(counter, seq, seq);
+        seq++;
     }
 
-    INFO("Skipping two packet sequence numbers: " << extended << ", " << extended + 1);
-    auto skipped = extended++;
-    extended++;
+    INFO("Skip two packet sequence numbers: " << seq << ", " << seq + 1);
+    CAPTURE(seq, counter);
+    auto skipped = seq++;
+    seq++;
 
-    // Send next few packets
-    INFO("Send a few packets " << counter);
+    INFO("Send a few more packets");
+    CAPTURE(seq, counter);
     for (int j = 0; j < 10; ++j)
     {
-        extend(counter, extended, extended);
-        extended++;
+        extend(counter, seq, seq);
+        seq++;
     }
 
-    INFO("Receive skipped packets (simulating NACK) " << counter << " extended:" << extended);
+    INFO("Receive skipped packets (simulating NACK re-transmits)");
+    CAPTURE(seq, counter);
     extend(counter, skipped, skipped);
     extend(counter, skipped + 1, skipped + 1);
 }
